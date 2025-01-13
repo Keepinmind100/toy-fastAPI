0a14aff7a756f9eb327463b1490685879db75b0c
keepinmind <pjwzzang190@naver.com>
2025-01-14 01:03:19 +0900

    3장 완료

---
+++ user/application/user_service.py
@@ -0,0 +1,43 @@
+from datetime import datetime
+from http.client import HTTPException
+from ulid import ULID
+from user.domain.repository.user_repo import IUserRepository
+from user.infra.repository.user_repo import UserRepository
+from user.domain.user import User
+from util.crypto import Crypto
+
+
+
+class UserService:
+    def __init__(self, user_repository):
+        self.user_repo: IUserRepository = UserRepository()
+        self.ulid = ULID() 
+        self.crypto = Crypto()
+    
+    def create_user(self, name:str, email:str, password:str):
+
+        # 중복 유저 검사
+        _user = None
+        
+        try:
+            _user = self.user_repo.find_by_email(email)
+        except HTTPException as e:
+            if e.status_code != 422:
+                raise e
+        if _user:
+            raise HTTPException(status_code=422, detail="Email already exists")
+        
+        now = datetime.now()
+        user: User = User(
+            id=self.ulid.generate(), 
+            name=name,
+            email=email,
+            password=self.crypto.encrypt(password),
+            created_at=now,
+            updated_at=now
+        )
+
+        self.user_repo.save(user)
+
+        return _user
+ 
